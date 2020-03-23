cb31ad874589e71d0b376de28ed7646efeff6a3c
ciamac <hgzguzg>
2020-03-23 10:41:02 +0100

    Verified by Cia v1.1.1

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var app = express();
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false}));
+
+app.get('/',(req,res) =>{
+res.sendStatus(200).send("Der Server Funktioniert")       
+ 
+ })
+
+ app.get('/api', (req,rea)=>{
+       res.json([{user: 'fake', password:'fake'}])
+ })
+
+ module.exports = app;
