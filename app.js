89ebbb961323edfe91b58e4bbecf41b753ffd4d1
ciamac <hgzguzg>
2020-03-23 19:21:39 +0100

    Verified5

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -10,16 +9,16 @@ var patientRouter = require('./routes/patients');
 
 var app = express();
 
-app.use(logger('dev'));
+// app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/api', apiRouter);
-app.use('/patients', patientRouter)
+app.use('/',            indexRouter);
+app.use('/users',       usersRouter);
+app.use('/api',         apiRouter);
+app.use('/patients',    patientRouter)
 
 
 module.exports = app;
